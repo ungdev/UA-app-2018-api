f81b393191e8083789130da4945e28f4aa00562f
Axel Mousset <axel@buckless.com>
2018-06-20 13:00:34 +0200

    fix: cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ mongoose.connect(config.uri);
 
 const app     = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cookieSession({
     name: 'session',
     keys: ['jeanmichelMux1234']
