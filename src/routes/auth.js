9bdcfeead153ff58bd02f6d369409a63e4d3cc58
Axel Mousset <axel@buckless.com>
2018-06-20 10:38:35 +0200

    refact: login json status

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,14 +9,14 @@ router.get('/', (req, res) => {
         res
             .status(200)
             .json({
-                status: 'session cookie set'
+                logged: true
             });
     } else {
         res.cookie('token', '')
         res
             .status(200)
             .json({
-                status: 'session cookie not set'
+                logged: false
             });
     }
 });
